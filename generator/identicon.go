42a3035eac391c0f055084ca7809758415bf2d8c
GianluigiMemoli <g.memoli98@gmail.com>
2019-08-12 00:08:51 +0200

    Background rendering function switched to a more efficient one

--- generator/identicon.go
+++ generator/identicon.go
@@ -3,7 +3,8 @@ package generator
 import (
 	"image"
 	"image/color"
-	"log"
+	"image/png"
+	"os"
 )
 
 const (
@@ -16,9 +17,32 @@ type identicon struct {
 	cell            int
 	backgroundColor color.RGBA
 	foregroundColor color.RGBA
-	asImage         *image.RGBA
+	image           *image.RGBA
 	marginX         int
 	marginY         int
+}
+
+func New(size int) *identicon{
+	//Defining cell's side length = min(halfW/_COLS, halfH/_ROWS)
+	//Half because the image is written on the first half than is mirrored
+	cell := setCellSide(size)
+
+	//Make an image
+	img := initImg(size)
+	marginX, marginY := getMargins(size, cell)
+	return &identicon{
+		size:         	 size,
+		cell:            cell,
+		backgroundColor: color.RGBA{
+			R: 255,
+			G: 255,
+			B: 255,
+			A: 0xff,
+		},
+		image:   img,
+		marginX: marginX,
+		marginY: marginY,
+	}
 }
 
 func setCellSide(size int) int {
@@ -41,36 +65,19 @@ func initImg(size int) *image.RGBA{
 	return image.NewRGBA(image.Rectangle{ptTop, ptBtm})
 }
 
-func New(size int, fg color.RGBA) *identicon{
-	//Defining cell's side length = min(halfW/_COLS, halfH/_ROWS)
-	//Half because the image is written on the first half than is mirrored
-	log.Printf("Color fg: %v", fg)
-	cell := setCellSide(size)
-	//Make an image
-	img := initImg(size)
-	marginX, marginY := SetPadding(size, cell)
-	log.Printf("cell: %d\n", cell)
-	return &identicon{
-		size:         	 size,
-		cell:            cell,
-		backgroundColor: color.RGBA{
-			R: 255,
-			G: 255,
-			B: 255,
-			A: 0xff,
-		},
-		foregroundColor:	fg,
-		asImage: img,
-		marginX: marginX,
-		marginY:marginY,
-	}
+
+func getMargins(size, cell int) (marginX, marginY int){
+	// left/right margin
+	marginX = size/2 - (cell * _COLS)/2
+	// top/bottom margin
+	marginY = size/2 - (cell * _ROWS)/2
+	return
 }
 
 func (icon *identicon) renderCell(x, y int, aColor color.RGBA) (availableX int){
-	//Render a cell given x,y and returns last x available
-	img := icon.asImage
+	//render a cell given x,y and returns last x available
+	img := icon.image
 	var i,j int
-	log.Printf("Printing a cell x:%d y:%d", x, y)
 	for i = x; i - x <= icon.cell; i++ {
 		for j = y; j - y <= icon.cell; j++ {
 			img.SetRGBA(i, j, aColor)
@@ -79,66 +86,69 @@ func (icon *identicon) renderCell(x, y int, aColor color.RGBA) (availableX int){
 	return i
 }
 
-func (icon *identicon) MirrorHorizontally(){
-	img := icon.asImage
+func (icon *identicon) mirrorHorizontally(){
+	img := icon.image
 	halfSize := icon.size/2
 	for x := icon.marginX; x < halfSize; x++{
-		for y := icon.marginX; y < icon.size; y++ {
+		for y := icon.marginY; y < icon.size; y++ {
 			img.SetRGBA(icon.size - x, y, img.RGBAAt(x, y))
 		}
 	}
 }
 
-func SetPadding(size, cell int) (marginX, marginY int){
-	/*usedArea := (icon.cell * icon.cell) * (_COLS * _ROWS)
-	unusedArea := (icon.size * icon.size) - usedArea
-	//Consider unusedArea as 4 rectangle, one per side
-	rectArea := unusedArea / 4
-	rectHeight := rectArea / icon.size
-	//Adding 3 squares that are lost in the marginX composition
-	rectArea += (rectHeight * icon.size)
-	rectHeight = rectArea / icon.size*/
-	marginX = size/2 - (cell * _COLS)/2
-	//Top margin
-	usedHeight := cell * _ROWS
-	marginY = size/2 - usedHeight/2
 
-	log.Printf("MarginX: %d MarginY: %d", marginX, marginY)
-
-	return
-}
-
-func (icon *identicon) GetImg()  *image.RGBA{
-	return icon.asImage
-}
-
-func (icon *identicon) Render(hash []uint8) {
+func (icon *identicon) render(hash []uint8) {
 	hashSliced := hash[3:]
 	var x, y int
 	x = icon.marginX
 	y = icon.marginY
-	log.Printf("topmargin: %d", y)
 	currByte := 0
 	for i := 0; i < _ROWS; i++ {
 		for j := 0; j < _COLS/2; j++ {
-			log.Printf("Considering byte: %d", hashSliced[currByte])
 			if hashSliced[currByte]%2 == 0 {
 				x = icon.renderCell(x, y, icon.foregroundColor)
 			} else {
 				x = icon.renderCell(x, y, icon.backgroundColor)
 			}
 			currByte++
-
 		}
 		x = icon.marginX
 		y += icon.cell
 	}
 }
 
-func (icon *identicon) RenderBackground(){
-	for x := 0; x < icon.size/2; x++ {
-		for y := 0; y < icon.size; y++ {
-
+func (icon *identicon) renderBackground() {
+	img := icon.image
+	//drawing top & bottom background
+	for x := 0; x < icon.size; x++ {
+		for y := 0; y < icon.marginY; y++ {
+			img.SetRGBA(x, y, icon.backgroundColor)
+			img.SetRGBA(x, icon.size-y, icon.backgroundColor)
 		}
 	}
+	//drawing left/right background
+	for y := icon.marginY; y < icon.size-icon.marginY; y++ {
+		for x:= 0; x < icon.marginX; x++ {
+			img.SetRGBA(x, y, icon.backgroundColor)
+			img.SetRGBA(icon.size - x, y, icon.backgroundColor)
+		}
+	}
+}
+
+
+
+func (icon *identicon) Create(hash []uint8, fileName string) interface{} {
+	//Using first 3 hash bytes for color
+	icon.foregroundColor = color.RGBA{hash[0], hash[1], hash[2], 255}
+	icon.render(hash)
+	//icon.renderBackground()
+	icon.renderBackground()
+	icon.mirrorHorizontally()
+	f, err := os.Create(fileName+".png")
+	if err != nil {
+		return err
+	} else {
+		err = png.Encode(f, icon.image)
+	}
+	return err
 }
--- main.go
+++ main.go
@@ -4,29 +4,49 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"github.com/GianluigiMemoli/avatarme/generator"
-	"image/color"
-	"image/png"
 	"os"
+	"strconv"
 )
+
+func makeHash(usrInputString string) []byte {
+	h := sha1.New()
+	h.Write([]byte(usrInputString))
+	hashed := h.Sum(nil)
+	return hashed
+}
+
 
 func main(){
 
-	toBeHashed := "Gianluigi Memoli"
-	h := sha1.New()
-	h.Write([]byte(toBeHashed))
-	hashed := h.Sum(nil)
-	id := generator.New(256, color.RGBA{hashed[0], hashed[1], hashed[2], 0xff})
-	id.Render(hashed)
-	id.MirrorHorizontally()
-
-	f, err := os.Create("image.png")
-	if err != nil {
-		fmt.Printf("Error Occurred: %v", err)
-	} else {
-		png.Encode(f, id.GetImg())
+	args := os.Args
+	if len(args[1:]) != 3 {
+		fmt.Println("BAD INPUT\n" +
+			"********************************************" +
+			"\navatarme produces a squared image that is the graphical representation of an hash, so \n" +
+			"run: avatarme <word to represent> <size of square side> <filename output>\n" +
+			"\nside MUST be at least 128!\n\n"+
+			"********************************************")
+		return
+	}
+	wordToHash := args[1]
+	sideStr := args[2]
+	fileName := args[3]
+	side, err := strconv.Atoi(sideStr)
+	if err != nil || side < 128{
+		fmt.Println(
+			"BAD INPUT\n" +
+			"****************************************\n"+
+			"Provided a side value < 128 or non numeric!")
+		return
 	}
 
 
+	icon := generator.New(side)
+	errCreating := icon.Create(makeHash(wordToHash), fileName)
+	if errCreating != nil {
+		fmt.Printf("Error occurred: %s", err)
+	}
+	fmt.Printf("SUCCESS! \n Graphical Hash of %s is in file %s in dim of %d", wordToHash, fileName, side)
 }
 
 
